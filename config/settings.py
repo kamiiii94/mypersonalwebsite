cbf42b5c1d05761c7e801b5d72010b4e4b7bbdf0
kamiiii94 <kamranysaeed2013@gmail.com>
2025-08-19 08:55:34 +0330

    changing base page

--- config/settings.py
+++ config/settings.py
@@ -116,10 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    BASE_DIR / "portfolio" / "static",
+    os.path.join(BASE_DIR, "portfolio/static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
